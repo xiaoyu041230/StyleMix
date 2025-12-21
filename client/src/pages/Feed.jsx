4d1dd32a648646e9998f742534e340d3c33624d8
xiaoyu041230 <xiaoyuju04@gmail.com>
2025-12-20 22:31:11 -0500

    post feed and sponsor

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import Storiesbar from '../components/Storiesbar'
+import PostCard from '../components/PostCard'
 
 const Feed = () => {
 
@@ -22,16 +23,21 @@ const Feed = () => {
       
       {/* Stories and post list */}
       <div>
-        <Storiesbar />
+        <Storiesbar /> 
         <div className='p-4 space-y-6'>
-          List of post
+          {feeds.map((post)=>(
+            <PostCard key={post._id} post={post}/>
+          ))} 
         </div>
       </div>
 
       {/* Right Sidebar */}
-      <div>
-        <div>
-          <h1>Sponsored</h1>
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
         </div>
         <h1>Recent messages</h1>
       </div>
