e152ee28a0f01ccc4f60eb8230b5b96b145aa507
xiaoyu041230 <xiaoyuju04@gmail.com>
2026-02-15 23:07:44 +0800

    fixed inngest

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import {inngest, functions} from './innjest/index.js';
+import {inngest, functions} from './inngest/index.js';
 import {serve} from 'inngest/express'
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
 import postRouter from './routes/postRoutes.js';
 import itemRouter from "./routes/itemRoutes.js";
 import storyRouter from './routes/storyRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 const app = express();
 await connectDB();
@@ -23,6 +24,7 @@ app.use('/api/user', userRouter)
 app.use('/api/post', postRouter)
 app.use("/api/item", itemRouter)
 app.use('/api/story', storyRouter)
+app.use('/api/message', messageRouter)
 
 const PORT = process.env.PORT || 4000;
 
