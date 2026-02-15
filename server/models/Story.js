bada7731d197a251147ab2bdbdc67d4a10f061dd
xiaoyu041230 <xiaoyuju04@gmail.com>
2026-02-15 23:26:11 +0800

    item schema fixed

--- server/models/Story.js
+++ server/models/Story.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import itemSchema from './item.schema.js';
 
 const storySchema = new mongoose.Schema({
     user: {type: String, ref: 'User', required: true },
     content: {type: String },
     media_url: {type: String },
+    item_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Item', default: null },
     media_type: {type: String, enum: ['image', 'video', 'item', 'item_with_image', 'item_with_video'], required: true,},
     views_count: [{type: String, ref: 'User'}],
     background_color: { type: String  },
